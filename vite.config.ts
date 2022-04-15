2baa170f84eb3de343c670bd7578f338abf14c4e
Nuno Góis <github@nunogois.com>
2022-04-15 03:04:23 +0100

    rename output files to index

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'proxy-client-vue',
-      fileName: format => `proxy-client-vue.${format}.js`
+      fileName: format => `index.${format}.js`
     },
     rollupOptions: {
       external: ['vue'],
